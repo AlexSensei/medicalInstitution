4a5ac4b2f8ece2dbaabac8988420a951b50fc779
AlexSensei <antishadow15@gmai.com>
2018-07-25 13:09:15 +0200

    #AAKT-4 Arrays, JSON, classes and OOP in JS

---
+++ two/app.js
@@ -0,0 +1,74 @@
+//JSON vs Array in JS
+var personJSON = { "name":"John", "age":30, "car":null };
+var personsJSON = [{ "name":"John", "age":30, "car":null }, { "name":"Smith", "age":20, "car":"Nisan" } ];
+
+
+//Kreiranje klase 
+function Person (first_name, last_name, gender) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.gender = gender;
+}
+
+//Instanciranje klase
+var osoba = new Person('John', 'Smith', 'Male');
+
+//Nasledjivanje klase
+function Worker (first_name, last_name, gender, company) {
+    Person.call(this, first_name, last_name, gender);
+
+    this.company = company;
+}
+
+//Dodavanje staticke varijable i metode u klasu Person
+Person.job = 'jobless';
+
+Person.work = function(){
+    console.log('Work Work!');
+}
+//Kreiranje nove osobe da bi se test varijabla i funkcija
+var osoba2 = new Person('Mark', 'Logic', 'Male');
+
+Person.work();
+console.log(Person.job);
+
+//Promise
+
+var coffeeState = 'Hot';
+
+var isProgrammerEfficient = new Promise(
+    function(resolve, reject) {
+        if(coffeeState=='Hot') {
+            var programmer = {
+                state: 'Happy',
+                efficiency: '150%'
+            };
+            resolve(programmer);
+        } else {
+            var reason = new Error('Coffee is cold.');
+            reject(reason);
+        }
+    }
+)
+
+var tryCoffee = function () {
+    isProgrammerEfficient
+    .then(function (fulfilled) {
+        console.log(fulfilled);
+    })
+    .catch(function (error) {
+        console.log(error.message);
+    })
+}
+
+tryCoffee();
+
+function doHomework(subject, callback) {
+    alert(`Starting my ${subject} homework.`);
+    callback();
+}
+function alertFinished() {
+    alert('Finished my homework');
+}
+
+doHomework('math', alertFinished);
